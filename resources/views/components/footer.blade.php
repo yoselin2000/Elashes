eac2946d5e63c44d7a2373642986e2c32c9defc0
yoselin2000 <yoselingomm@gmail.com.com>
2024-10-08 15:48:02 -0400

    kui-breeze

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="flex-shrink-0 px-6 py-4">
+    <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
+        <span>Todos los derechos reservados</span>
+
+        <span>
+            <!-- <x-heroicon-s-heart class="w-6 h-6 text-red-500" /> -->
+        </span>
+
+        <a
+            href="https://github.com/yoselin2000"
+            target="_blank"
+            class="text-blue-600 hover:underline"
+        >jgv
+        </a>
+    </p>
+</footer>
